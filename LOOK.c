7a9be15bf36650269506ae312b0dc0928441ec5c
Shashankdk23 <117855805+Shashankdk23@users.noreply.github.com>
2023-08-16 15:45:30 +0530

    Add files via upload

---
+++ LOOK.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n, head,max,min,i;
+    char direction;
+    int sum = 0;
+
+    printf("SCAN\n");
+    printf("Enter the number of requests (n):");
+    scanf("%d", &n);
+
+    int rs[n];
+    printf("Enter request sequence one by one (in ascending)\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &rs[i]);
+    }
+
+    printf("Enter initial head position (p):");
+    scanf("%d", &head);
+
+    printf("Enter the direction(l or r)");
+    scanf(" %c", &direction);
+
+            max=rs[0];
+     for (i=0;i<n;i++)
+    {
+        if (rs[i] > max)
+        {
+                max=rs[i];
+        }
+    }
+     min=999;
+         for (i=0;i<n;i++)
+    {
+        if (rs[i]<min)
+        {
+                min=rs[i];
+        }
+    }
+
+    if (direction == 'l')
+    {
+
+        sum = (head-min)+(max-min);
+        printf("Total Seek Time: %d\n", sum);
+    }
+
+    else if (direction == 'r')
+    {
+       sum = (max-head)+(max-min);
+        printf("Total Seek Time: %d\n", sum);
+    }
+
+    else
+    {
+        printf("invalid input try l or r");
+    }
+    return 0;
+}
